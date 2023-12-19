073430557ea47ff0c16e3daa7770fcb8d91b4eda
thouel <t.houel@gmail.com>
2023-12-19 22:05:58 +0100

    func: initial skeleton

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { withLogtail } = require('@logtail/next')
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+    ],
+  },
+  compiler: {
+    removeConsole: false,
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  experimental: {
+    typedRoutes: true,
+  },
+}
+
+module.exports = withLogtail(nextConfig)
